ac1f5443a36949f17e828ec19101b3994e5af299
shouyamakichi <houyama5610@gmail.com>
2020-08-28 20:56:57 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :name, presence: true
+  has_many :rooms_users
+  has_many :rooms, through: :rooms_users
 end
